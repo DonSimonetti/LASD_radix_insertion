a0a09092c930b958d5bfc81adae3ee98799a07c1
DonSimonetti <matt.simonetti@hotmail.com>
2022-06-22 15:18:14 +0200

    Updated run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,2 @@
-time python test_random_numbers.py 4 > output_$(date +%Y-%m-%d-%H-%M-%S).log
+time python test_random_numbers.py 5 > random_numbers_output_$(date +%Y-%m-%d-%H-%M-%S).log
+time python test_almost_sorted_arrays.py 5 > almost_sorted_output_$(date +%Y-%m-%d-%H-%M-%S).log
